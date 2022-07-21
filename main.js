a39137c03c375aac18d1b4b7d30486d3343bfa9f
Andrew LaRue <pikey6957@gmail.com>
2022-07-21 16:59:57 -0600

    save

--- main.js
+++ main.js
@@ -70,3 +70,93 @@ function drawMonsterImg(){
 }
 drawMonsterImg()
 
+function drawHeroStats() {
+  let template = ''
+  for (let key in heroes) {
+    let hero = heroes[key]
+    template += `
+          <div class="col-4">
+              ${key}
+            </div>
+            <div class="col-6">
+              <div class="progress">
+                <div class=" hero-bar progress-bar" role="progressbar" style="width: ${hero.health}%;" aria-valuenow="" aria-valuemin="0"
+                aria-valuemax="100">${hero.health}%</div>
+                </div>
+              </div>
+            <div class="col-2">
+            <img class="mini-img" src="/assets/Potion.gif" alt="">
+          </div>
+    `
+    let heroElm = document.getElementById('heroStats')
+    heroElm.innerHTML = template
+  }
+}
+drawHeroStats()
+function drawMonsterStats() {
+  let template = ''
+  for (let key in monsters) {
+    let monster = monsters[key]
+    template += `
+            <div class="progress">
+          <div class="monster-bar progress-bar" role="progressbar" style="width: ${monster.health}%;" aria-valuenow="25" aria-valuemin="0"
+            aria-valuemax="100">${monster.health}%</div>
+        </div>
+        <p class="fs-2 text-center">${key}</p>
+    `
+    let monsterElm = document.getElementById('monsterStats')
+    monsterElm.innerHTML = template
+  }
+}
+drawMonsterStats()
+
+function attack() {
+  let monster = monsters.bonedog
+  monster.health -= 5
+  if (monster.health < 0) {
+    monster.health = 0
+  }
+  let monsterElm = document.getElementById('monsterStats')
+  let healthBar = monsterElm.querySelector('.monster-bar') 
+  healthBar.style.width = `${monster.health}%`
+  healthBar.innerHTML = monster.health
+}
+
+function bossAttack() {
+  for (let key in heroes) {
+    let hero = heroes[key]
+    hero.health -= 10
+    if (hero.health < 0) {
+      hero.health = 0
+    }
+    drawHeroStats()
+  }
+}
+
+function reset() {
+// location.reload()
+  resetMonster()
+  resetHero()
+  
+}
+
+function resetMonster() {
+  let monster = monsters.bonedog
+  monster.health = 100
+    let monsterElm = document.getElementById('monsterStats')
+  let healthBar = monsterElm.querySelector('.monster-bar') 
+  healthBar.style.width = `${monster.health}%`
+  healthBar.innerHTML = monster.health
+}
+function resetHero() {
+  let hero = heroes.soldier76
+  hero.health = 100
+    let heroElm = document.getElementById('heroStats')
+  let healthBar = heroElm.querySelector('.hero-bar') 
+  healthBar.style.width = `${hero.health}%`
+  healthBar.innerHTML = hero.health
+}
+
+
+
+setInterval(bossAttack, 2000)
