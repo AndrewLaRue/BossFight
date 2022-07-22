e8056a7b00b3fc101d00c7138f881a89379cafe4
Andrew LaRue <pikey6957@gmail.com>
2022-07-22 00:02:26 -0600

    done (ish)

--- main.js
+++ main.js
@@ -22,16 +22,24 @@ const heroes = {
 }
 
 const monsters = {
-    bonedog:{
-        health: 100,
-        attack: 25,
-        img: 'assets/Monster BoneDog.gif'
-    },
-    // threeeyes:{
+    // bonedog:{
     //     health: 100,
     //     attack: 25,
-    //     img: 'assets/Monster Eye.gif'
+    //     img: 'assets/Monster BoneDog.gif',
+    //     level: 1
     // },
+    // MurderTree:{
+    //     health: 100,
+    //     attack: 25,
+    //     img: 'assets/Monster Tree.gif',
+    //     level: 1
+    // },
+    TriEye:{
+        health: 100,
+        attack: 25,
+        img: 'assets/Monster Eye.gif',
+        level: 1
+    },
     // ghost:{
     //     health: 100,
     //     attack: 25,
@@ -44,88 +52,105 @@ const monsters = {
     // },
 }
 
+const companions = {
+  wolf: {
+    rounds: 0,
+    img: 'assets/Companion Wolf.gif'
+  }
+}
+
+let gold = 50
+
+function startGame() {
+drawHeroImg()
+drawMonsterImg()
+drawMonsterStats()
+  drawHeroStats()
+  setInterval(bossAttack, 2000)
+}
 
 function drawHeroImg(){
     let template = ''
     for(let key in heroes){
         let hero = heroes[key]
-    template += `
-    <img class="img-fluid" src="/assets/Hero gun.gif" alt="">`
+      template += `<img class="hero-size" src="/assets/Hero gun.gif" alt="">`
     let heroElm = document.getElementById('hero')
     heroElm.innerHTML = template
 }
 }
-drawHeroImg()
+
 
 function drawMonsterImg(){
     let template = ''
     for(let key in monsters){
         let monster = monsters[key]
     template += `
-    <img class="monster-size" src="/assets/Monster BoneDog.gif" alt="">
+    <img class="monster-size" src="/assets/Monster Eye.gif" alt="">
     `    
     let monsterElm = document.getElementById('monster')
     monsterElm.innerHTML = template   
 }
 }
-drawMonsterImg()
+
 
 function drawHeroStats() {
   let template = ''
   for (let key in heroes) {
     let hero = heroes[key]
     template += `
-          <div class="col-4">
+          <div class="col-3">
               ${key}
-            </div>
-            <div class="col-6">
+          </div>
+          <div class="col-9 mt-2">
               <div class="progress">
-                <div class=" hero-bar progress-bar" role="progressbar" style="width: ${hero.health}%;" aria-valuenow="" aria-valuemin="0"
+                <div class="hero-bar progress-bar" role="progressbar" style="width: ${hero.health}%;" aria-valuenow="" aria-valuemin="0"
                 aria-valuemax="100">${hero.health}%</div>
-                </div>
               </div>
-            <div class="col-2">
-            <img class="mini-img" src="/assets/Potion.gif" alt="">
           </div>
     `
     let heroElm = document.getElementById('heroStats')
     heroElm.innerHTML = template
   }
 }
-drawHeroStats()
+
 function drawMonsterStats() {
   let template = ''
   for (let key in monsters) {
     let monster = monsters[key]
     template += `
-            <div class="progress">
-          <div class="monster-bar progress-bar" role="progressbar" style="width: ${monster.health}%;" aria-valuenow="25" aria-valuemin="0"
-            aria-valuemax="100">${monster.health}%</div>
-        </div>
-        <p class="fs-2 text-center">${key}</p>
+            <div class="progress mt-2">
+              <div class="monster-bar progress-bar" role="progressbar" style="width: ${monster.health}%;">${monster.health}%</div>
+            </div>
+            <p class="d-flex align-items-center justify-content-between fs-2 mb-0 pb-0">${key}<span class="fs-5 level"> Level: <span class="level">${monster.level}</span></span></p>
     `
     let monsterElm = document.getElementById('monsterStats')
     monsterElm.innerHTML = template
   }
 }
-drawMonsterStats()
+
 
 function attack() {
-  let monster = monsters.bonedog
-  monster.health -= 5
-  if (monster.health < 0) {
-    monster.health = 0
+  let monster = monsters.TriEye
+  let atk = Math.floor(Math.random()*5)
+  monster.health -= atk
+  if (monster.health <= 0) {
+    monster.level++
+    gold += monster.level*10
+    monster.health = 100* monster.level
   }
-  let monsterElm = document.getElementById('monsterStats')
-  let healthBar = monsterElm.querySelector('.monster-bar') 
-  healthBar.style.width = `${monster.health}%`
-  healthBar.innerHTML = monster.health
+  // let monsterElm = document.getElementById('monsterStats')
+  // let healthBar = monsterElm.querySelector('.monster-bar') 
+  // healthBar.style.width = `${monster.health}%`
+  // healthBar.innerHTML = monster.health
+  drawMonsterStats()
+  wolfAtk()
 }
 
 function bossAttack() {
   for (let key in heroes) {
     let hero = heroes[key]
-    hero.health -= 10
+    let atk = Math.floor(Math.random()*3)
+    hero.health -= atk*monsters.TriEye.level
     if (hero.health < 0) {
       hero.health = 0
     }
@@ -141,12 +166,15 @@ function reset() {
 }
 
 function resetMonster() {
-  let monster = monsters.bonedog
+  let monster = monsters.TriEye
   monster.health = 100
+  monster.level = 1
     let monsterElm = document.getElementById('monsterStats')
   let healthBar = monsterElm.querySelector('.monster-bar') 
   healthBar.style.width = `${monster.health}%`
   healthBar.innerHTML = monster.health
+  let level = monsterElm.querySelector('.level')
+  level.innerHTML = monster.level
 }
 function resetHero() {
   let hero = heroes.soldier76
@@ -157,6 +185,48 @@ function resetHero() {
   healthBar.innerHTML = hero.health
 }
 
+function buyPotion() {
+  let hero = heroes.soldier76
+  if (gold >= 25) {
+    gold -= 25
+    hero.health += 25
+    if (hero.health > 100) {
+      hero.health = 100
+    }
+        let heroElm = document.getElementById('heroStats')
+  let healthBar = heroElm.querySelector('.hero-bar') 
+  healthBar.style.width = `${hero.health}%`
+  healthBar.innerHTML = hero.health
+  }
+    
+}
+
+//#region  testing
 
 
-setInterval(bossAttack, 2000)
+function buyWolf() {
+  if (gold >= 50) {
+    gold -= 50
+    companions.wolf.rounds += 10
+  }
+}
+
+function drawWolf() {
+  
+}
+
+function wolfAtk() {
+  let dog = companions.wolf
+  let atk = Math.floor(Math.random()*5)
+  if (dog.rounds > 0) {
+    dog.rounds--
+    monsters.TriEye.health -= atk
+    drawMonsterStats()
+    
+}
+}
+
+
+//#endregion
+
+startGame()
